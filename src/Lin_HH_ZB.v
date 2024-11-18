8a387a444733092880ad7c11027273a7e60dbf6e
Bethel, Zack <zbethel@ucsc.edu>
2024-11-17 16:15:06 -0800

    testing all things with example lif

--- src/Lin_HH_ZB.v
+++ src/Lin_HH_ZB.v
@@ -1,7 +1,7 @@
 module LinearHodgkinHuxleyModel (
     input wire          clock,          // Clock input
     input wire          reset,          // Reset input
-    input wire [15:0]   current_in,     // Multiplexed input for I (current)
+    input wire [15:0]   current_in,     // I (current)
     input wire [15:0]   dt,             // and dt (time step)
     output wire         spike
     );
@@ -12,25 +12,24 @@ module LinearHodgkinHuxleyModel (
     wire I_NA, I_K, I_L, I_tot, V_membrane, dt_time;
     reg V_next, C_M;
 
-  	assign dt_time = dt;
     // Currents
     sodiumCurrent NA (.clk(clock), .rst(reset), .dt(dt), .V(V_membrane), .I_NA(I_NA));
     potassiumCurrent K (.clk(clock), .rst(reset), .dt(dt), .V(V_membrane), .I_K(I_K));
     leakCurrent L (.clk(clock), .rst(reset), .V(V_membrane), .I_L(I_L));
 
     // Calculate next membrane potential using discretized equation
-  always @(posedge clock) begin
+    always @(posedge clock) begin
 
-        C_M <= 1e-6;
-        threshold <= 16'd30;
-    if (reset) begin
-            V_next <= -65;  // Initial membrane potential
-        end else begin
-          V_next <= V_next + dt * (current_in - (I_NA + I_K + I_L)) / C_M;
-        end
-    end
+          C_M <= ;
+          threshold <= 16'd30;
+      if (reset) begin
+              V_next <= -65;  // Initial membrane potential
+          end else begin
+            V_next <= V_next + dt * (current_in - (I_NA + I_K + I_L)) / C_M;
+          end
+      end
 
-    //spiking logic
-    assign spike = (V_next >= threshold);
+      //spiking logic
+      assign spike = (V_next >= threshold);
 
 endmodule
--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_lif_ZB user_project (
+  tt_um_lif_ZB_copy user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
