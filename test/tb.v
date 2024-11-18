e96ba7e77583ca7405d49549b4f40b7dbde291fa
Bethel, Zack <zbethel@ucsc.edu>
2024-11-17 16:40:30 -0800

    Attempt 1 - testing HH Neuron after succesful test with LIF

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,8 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_lif_ZB_copy user_project (
+  // tt_um_lif_ZB_copy user_project ( //Example LIF neuron
+  tt_um_lif_ZB user_project ( //HH neuron
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
